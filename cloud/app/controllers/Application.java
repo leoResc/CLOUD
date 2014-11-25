d4f6f1db9a53d0d8b357f16f74ab3078757bd9c2
Philipp <philippus-perez@web.de>
2014-11-25 23:58:21 +0100

    generated methos for login and createEvent

--- cloud/app/controllers/Application.java
+++ cloud/app/controllers/Application.java
@@ -7,8 +7,12 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    public static Result login() {
+        return ok(index.render());
+    }
+
+    public static Result adminCreateEvent() {
+        return ok(createEvent.render());
     }
 
 }
