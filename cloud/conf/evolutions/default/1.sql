093c27159841c0f2c62db7cde558f9b862135fc7
Philipp <philippus-perez@web.de>
2014-11-28 00:33:28 +0100

    added database

---
+++ cloud/conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table event (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  password                  varchar(255),
+  description               varchar(255),
+  begin                     datetime,
+  end                       datetime,
+  constraint pk_event primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table event;
+
+SET FOREIGN_KEY_CHECKS=1;
+
