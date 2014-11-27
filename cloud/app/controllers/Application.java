d41f56d4ebfdd48c74fa3c43b75af5bb1f2e6adc
Philipp <philippus-perez@web.de>
2014-11-28 00:33:09 +0100

    implemented first test to retrieve data from form

--- cloud/app/controllers/Application.java
+++ cloud/app/controllers/Application.java
@@ -1,23 +1,29 @@
 package controllers;
 
 import models.Event;
-import play.*;
-import play.mvc.*;
 import views.html.*;
+import play.Logger;
+import play.data.Form;
+import play.mvc.*;
 
 public class Application extends Controller {
 
-    public static Result login() {
-        return ok(login.render());
-    }
+	public static Result index() {
+		return ok(index.render());
+	}
 
-    public static Result createEvent() {
-    	Event event = new Event();
-        return ok(createEvent.render(event));
-    }
-    
-    public static Result index() {
-    	return ok(index.render());
-    }
+	public static Result login() {
+		return ok(login.render());
+	}
 
+	public static Result createEvent() {
+		return ok(createEvent.render());
+	}
+
+	public static Result addEvent() {
+		Event event = Form.form(Event.class).bindFromRequest().get();
+		Logger.info("event received:" + System.lineSeparator() + event.name + System.lineSeparator() + event.description + System.lineSeparator() + event.password);
+		event.save();
+		return ok(index.render());
+	}
 }
