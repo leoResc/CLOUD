46fe83a729f0f39040325ab4d034ad4122c9879e
Philipp <philippus-perez@web.de>
2014-11-27 23:24:32 +0100

    set up database connection

--- cloud/app/models/Event.java
+++ cloud/app/models/Event.java
@@ -2,32 +2,19 @@ package models;
 
 import java.util.Calendar;
 
-public class Event {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import play.db.ebean.Model;
+
+@Entity
+public class Event extends Model {
+
+	@Id
+	public String id;
 	public String name;
 	public String password;
 	public String description;
 	public Calendar begin;
 	public Calendar end;
-	
-	public Event()  {
-		this.name = "Test";
-		this.password = "aligator7";
-		this.description = "Test-Party";
-		
-		this.begin = Calendar.getInstance();
-		this.begin.set(2014, 10, 26, 12, 24);
-
-		this.end = Calendar.getInstance();
-		this.end.set(2014, 10, 26, 13, 24);
-	}
-
-	public String getBegin() {
-		return this.begin.getTime().toString();
-	}
-	
-	public void setBegin(Calendar begin) {
-		this.begin = begin;
-	}
-	
 }
