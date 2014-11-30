0a3d978057e4af70217f0127500b954a3ed83ad6
Philipp <philippus-perez@web.de>
2014-11-30 17:42:31 +0100

    added function to dismiss alert

--- cloud/public/javascripts/login.js
+++ cloud/public/javascripts/login.js
@@ -2,9 +2,14 @@ document.getElementById("waves").volume = 0.4;
 
 function showLogin() {
 	$('.showLogin').click(function() {
-		$('.form-signin').show();
 		$('.showLogin').remove();
+		$('.title').animate({marginTop:'-100px'},800);
+		$('.form-signin').delay(100).fadeIn(800);
 	});
+}
+
+function dismiss() {
+	$('.alert').hide(400);
 }
 
 $(document).ready(showLogin);
