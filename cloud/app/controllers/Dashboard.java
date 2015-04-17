14feb7554bccff5497008616e52c6019a5b4b330
Philipp <philippus-perez@web.de>
2015-04-17 17:57:30 +0200

    filled new controller for dashboard
    CLOUD-60: Integration of Dashboard Design 
    
    Task-Url: http://jira.it.dh-karlsruhe.de:8080/browse/CLOUD-60

--- cloud/app/controllers/Dashboard.java
+++ cloud/app/controllers/Dashboard.java
@@ -9,4 +9,20 @@ public class Dashboard extends Controller {
 	public static Result getDashboard() {
 		return ok(dashboard.render());		
 	}
+	
+	public static Result getEvent() {
+		return ok(event.render());
+	}
+	
+	public static Result getPlaylist() {
+		return ok(playlist.render());
+	}
+	
+	public static Result getSong() {
+		return ok(song.render());
+	}
+	
+	public static Result getUser() {
+		return ok(user.render());
+	}
 }
