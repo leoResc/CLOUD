8b124a9964bc613c0b9abfa951d09dcc46925936
Philipp <philippus-perez@web.de>
2015-04-21 19:03:01 +0200

    CLOUD-44: Controller for data reception
    Task-Url: http://jira.it.dh-karlsruhe.de:8080/browse/CLOUD-44
    
    added controller for reception of upload with AJAX

--- cloud/app/controllers/Dashboard.java
+++ cloud/app/controllers/Dashboard.java
@@ -1,7 +1,14 @@
 package controllers;
 
+import java.util.List;
+
+
+
+import play.*;
+import models.Song;
 import play.mvc.Result;
 import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
 import views.html.*;
 
 public class Dashboard extends Controller {
@@ -24,5 +31,12 @@ public class Dashboard extends Controller {
 	
 	public static Result getUser() {
 		return ok(user.render());
+	}
+	
+	public static Result uploadSong() {
+		MultipartFormData body = request().body().asMultipartFormData();
+		List<MultipartFormData.FilePart> files = body.getFiles();
+		Song.uploadSong(files);
+		return ok();		
 	}
 }
