a2400d75f69b48f9304e33fb204d5b661e515846
philenius <philippus-perez@web.de>
2015-05-08 19:28:59 +0200

    CLOUD-70: AJAX communication
    Task-Url: http://jira.it.dh-karlsruhe.de:8080/browse/CLOUD-70

--- cloud/public/javascripts/dashboard.js
+++ cloud/public/javascripts/dashboard.js
@@ -161,21 +161,70 @@ $(function () {
 });
 
 // clock
+var updated = false;
 $(document).ready(function() {
-    var serverTime = new Date();
-    serverTime.setHours($('.hour').text());
-    serverTime.setMinutes($('.minute').text());    
-    var clientTime = new Date();
-    if (Math.abs(clientTime - serverTime) > 600000) {
-        for (var i = 0; i < 15; i++) {
-            $('#time .infobox-footer').delay(700).animate({backgroundColor: '#E16874'}, 300);
-            $('#time .infobox-footer').animate({backgroundColor: '#435361'}, 300);
-        }
-    };
+    setInterval(warning, 2000);
 });
 
+function warning() {
+	if(!updated ) {
+		var serverTime = new Date();
+	    serverTime.setHours($('.hour').text());
+	    serverTime.setMinutes($('.minute').text());    
+	    var clientTime = new Date();
+	    if (Math.abs(clientTime - serverTime) > 600000) {
+    		$('#time .infobox-footer').delay(700).animate({backgroundColor: '#E16874'}, 300);
+	        $('#time .infobox-footer').animate({backgroundColor: '#435361'}, 300);
+	    };
+    };
+};
+
 $('#updateTime').click(function() {
-    var update = $(this).find('.glyphicon-download');
-    update.switchClass('glyphicon-download', 'glyphicon-refresh');
-    update.addClass('rotate');
+    $(this).find('.glyphicon-refresh').removeClass('glyphicon-refresh');
+});
+
+// update time
+$('#updateTime').click(function() {
+	var date = new Date();
+	$.ajax({
+		url : '/updateTime/' + date,
+		type : 'POST',
+		success : function(response) {
+			updated = true;
+			response = JSON.parse(response);
+			var date = new Date();
+			date.setTime(response);
+			$('.hour').text(date.getHours() <= 9 ? '0' + date.getHours():date.getHours());
+			$('.minute').text(date.getMinutes() <= 9 ? '0' + date.getMinutes():date.getMinutes());
+			var day;
+			switch(date.getDay()) {
+				case 1: day = 'Monday'; break;
+				case 2: day = 'Tuesday'; break;
+				case 3: day = 'Wednesday'; break;
+				case 4: day = 'Thursday'; break;
+				case 5: day = 'Friday'; break;
+				case 6: day = 'Satturday'; break;
+				case 7: day = 'Sunday'; break;
+			}
+			var month;
+			switch(date.getMonth()) {
+				case 0: month = 'January'; break;
+				case 1: month = 'February'; break;
+				case 2: month = 'March'; break;
+				case 3: month = 'April'; break;
+				case 4: month = 'May'; break;
+				case 5: month = 'June'; break;
+				case 6: month = 'July'; break;
+				case 7: month = 'August'; break;
+				case 8: month = 'September'; break;
+				case 9: month = 'October'; break;
+				case 10: month = 'November'; break;
+				case 11: month = 'December'; break;
+			}
+			$('.date').text(day + ', ' + month + ' ' + date.getDate() + ', ' + date.getFullYear());
+			$('#updateTime').find('.glyphicon').toggleClass('glyphicon-ok');
+			$('#time .infobox-footer').delay(700).animate({backgroundColor: '#38B89F'}, 300);
+	        $('#time .infobox-footer').animate({backgroundColor: '#435361'}, 300);
+		}
+	});
 });
