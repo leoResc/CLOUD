340db707a03699c471251e0416cc4502c5ab1f95
philenius <philippus-perez@web.de>
2015-05-09 20:02:06 +0200

    corrected spelling

--- cloud/public/javascripts/dashboard.js
+++ cloud/public/javascripts/dashboard.js
@@ -203,7 +203,7 @@ $('#updateTime').click(function() {
 				case 3: day = 'Wednesday'; break;
 				case 4: day = 'Thursday'; break;
 				case 5: day = 'Friday'; break;
-				case 6: day = 'Satturday'; break;
+				case 6: day = 'Saturday'; break;
 				case 7: day = 'Sunday'; break;
 			}
 			var month;
