7a3d7f11433969c4a149716f5f1742171494bb7f
philenius <philippus-perez@web.de>
2015-05-11 00:04:22 +0200

    corrected bug

--- cloud/public/javascripts/dashboard.js
+++ cloud/public/javascripts/dashboard.js
@@ -198,13 +198,13 @@ $('#updateTime').click(function() {
 			$('.minute').text(date.getMinutes() <= 9 ? '0' + date.getMinutes():date.getMinutes());
 			var day;
 			switch(date.getDay()) {
+				case 0: day = 'Sunday'; break;
 				case 1: day = 'Monday'; break;
 				case 2: day = 'Tuesday'; break;
 				case 3: day = 'Wednesday'; break;
 				case 4: day = 'Thursday'; break;
 				case 5: day = 'Friday'; break;
 				case 6: day = 'Saturday'; break;
-				case 7: day = 'Sunday'; break;
 			}
 			var month;
 			switch(date.getMonth()) {
