a9b687586858348783efc9071e1d4a45bac634c7
philenius <philippus-perez@web.de>
2015-05-27 10:14:51 +0200

    added error page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -127,7 +127,7 @@ public class Application extends Controller {
 			if (session.equals("admin")) {
 				return ok(createEvent.render());
 			} else {
-				return redirect(routes.Application.getIndex());
+				return badRequest(views.html.forbidden.render());
 			}
 		}
 	}
