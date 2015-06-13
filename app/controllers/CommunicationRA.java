37806ac6eb4077b9297f95e3c6b078f162f8e3a2
philenius <philippus-perez@web.de>
2015-06-13 20:53:30 +0200

    added playback of thunderstorm

--- app/controllers/CommunicationRA.java
+++ app/controllers/CommunicationRA.java
@@ -9,17 +9,24 @@ public class CommunicationRA extends Controller {
 	public static Result changeMode(int mode) {
 
 		if ((mode >= 0) & (mode <= 9)) {
-			ShellCommand command = new ShellCommand("python communicationArduino.py " + mode);
+			ShellCommand command = new ShellCommand(
+					"python communicationArduino.py " + mode);
 			command.executeShellCommand();
-			
-			if(mode == 0) {
+
+			if (mode == 0) {
 				command = new ShellCommand("sudo shutdown -h now");
 				command.executeShellCommand();
+			} else if (mode == 5) {
+				ShellCommand s1 = new ShellCommand("mpc clear");
+				s1.executeShellCommand();
+				ShellCommand s2 = new ShellCommand("mpc add Thunderstorm.mp3");
+				s2.executeShellCommand();
+				ShellCommand s3 = new ShellCommand("mpc play");
+				s3.executeShellCommand();
 			}
-
 			return ok();
 		}
-		
+
 		return badRequest();
 	}
 }
