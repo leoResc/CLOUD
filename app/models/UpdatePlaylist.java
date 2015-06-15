16d31325b2461db3f1b3f741312517f230cb6e68
philenius <philippus-perez@web.de>
2015-06-15 19:14:14 +0200

    removed debug messages

--- app/models/UpdatePlaylist.java
+++ app/models/UpdatePlaylist.java
@@ -11,26 +11,21 @@ public class UpdatePlaylist extends TimerTask {
 	public void run() {
 		ShellCommand sh = new ShellCommand("mpc status");
 		StringBuffer output = sh.executeShellCommand();
-		Logger.info("read full status:" + output);
 		int begin = output.indexOf("] #");
 		int end = output.indexOf("%)");
-		// substring from [playing] to %)
+		// substring from '] #' to '%)'
 		String mpcStatus = output.substring(begin, end + 2);
-		Logger.info("found in full status: " + mpcStatus);
 		if ((begin >= 0) && (end >= begin)) {
 			begin = mpcStatus.indexOf("(");
 			end = mpcStatus.indexOf("%");
 			try {
 				int percentage = Integer.valueOf(mpcStatus.substring(begin + 1,
 						end));
-				Logger.info("read percentage of song: " + percentage);
 				if (percentage >= 1 & percentage < 80) {
 					loadedNext = false;
-					Logger.info("loaded next: " + loadedNext);
 				} else if ((percentage >= 80) && (!loadedNext)) {
 					CurrentPlaylist.addNextSongToPlaylist();
 					loadedNext = true;
-					Logger.info("loaded next song");
 				}
 			} catch (NumberFormatException | StringIndexOutOfBoundsException e) {
 				Logger.error("error while parsing mpc status");
