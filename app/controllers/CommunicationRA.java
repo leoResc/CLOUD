974cd38ad8bdd61fb32b2d88dc035d272d276160
philenius <philippus-perez@web.de>
2015-06-15 16:38:46 +0200

    refactored communication between Arduino and Raspberry

--- app/controllers/CommunicationRA.java
+++ app/controllers/CommunicationRA.java
@@ -8,21 +8,26 @@ public class CommunicationRA extends Controller {
 
 	public static Result changeMode(int mode) {
 
-		if ((mode >= 0) & (mode <= 9)) {
-			ShellCommand command = new ShellCommand(
-					"python communicationArduino.py " + mode);
-			command.executeShellCommand();
+		if ((mode >= 0) & (mode <= 5)) {
 
+			// Shutdown
 			if (mode == 0) {
-				command = new ShellCommand("sudo shutdown -h now");
+				ShellCommand command = new ShellCommand("sudo shutdown -h now");
 				command.executeShellCommand();
-			} else if (mode == 5) {
-				ShellCommand s1 = new ShellCommand("mpc clear");
-				s1.executeShellCommand();
-				ShellCommand s2 = new ShellCommand("mpc add Thunderstorm.mp3");
-				s2.executeShellCommand();
-				ShellCommand s3 = new ShellCommand("mpc play");
-				s3.executeShellCommand();
+			} else {
+				ShellCommand command = new ShellCommand(
+						"python communicationArduino.py '" + mode + "'");
+				command.executeShellCommand();
+				// Thunderstorm
+				if (mode == 5) {
+					ShellCommand s1 = new ShellCommand("mpc clear");
+					s1.executeShellCommand();
+					ShellCommand s2 = new ShellCommand(
+							"mpc add Thunderstorm.mp3");
+					s2.executeShellCommand();
+					ShellCommand s3 = new ShellCommand("mpc play");
+					s3.executeShellCommand();
+				}
 			}
 			return ok();
 		}
