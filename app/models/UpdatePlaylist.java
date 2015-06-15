4545b5fba7a55e03d29427c4cd2ba4fb3a3ba010
philenius <philippus-perez@web.de>
2015-06-15 18:21:48 +0200

    added debug messages

--- app/models/UpdatePlaylist.java
+++ app/models/UpdatePlaylist.java
@@ -2,6 +2,8 @@ package models;
 
 import java.util.TimerTask;
 
+import com.sun.org.apache.bcel.internal.generic.LALOAD;
+
 import play.Logger;
 
 public class UpdatePlaylist extends TimerTask {
@@ -17,11 +19,14 @@ public class UpdatePlaylist extends TimerTask {
 			try {
 				int percentage = Integer.valueOf(output.substring(begin + 1,
 						end));
+				Logger.info("read percentage of song: " + percentage);
 				if (percentage >= 1 & percentage < 80) {
 					loadedNext = false;
+					Logger.info("loaded next: " + loadedNext);
 				} else if ((percentage >= 80) && (!loadedNext)) {
 					CurrentPlaylist.addNextSongToPlaylist();
 					loadedNext = true;
+					Logger.info("loaded next song");
 				}
 			} catch (NumberFormatException | StringIndexOutOfBoundsException e) {
 				Logger.error("error while parsing mpc status");
